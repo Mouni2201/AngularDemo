8108569d918ba5561021158174f2e4cb591ed53b
Chanda Mounica <pvryar.4@gmail.com>
2025-08-26 10:11:12 +0530

    ngmodel demo

---
+++ src/app/comp-a/comp-a.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-a',
+  templateUrl: './comp-a.component.html',
+  styleUrls: ['./comp-a.component.css']
+})
+export class CompAComponent {
+
+  title= 'Angular Demo';
+  username = 'John'; 
+
+
+   isSubscribed = true; 
+
+  selectedCity = 'blr'; 
+
+  
+}
+
+
+
